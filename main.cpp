583aa900fff61a15bcc60fdcd91cc67edefe50ec
akarola <akarola@idc.com>
2018-04-10 21:09:26 +0200

    working version of data parallelism

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-const int THRESHOLD = 5;
+const int THRESHOLD = 4;
 
-void recursionParallel(TaskInstance task, int x, int y, int treeLevel);
+void generateStatesRecursively(TaskInstance task, int x, int y, int treeLevel);
 void recursionSequential(TaskInstance task, pair<int, int> queenNewPosition);
 
 void printMoves(vector<pair<int, int>> &moves);
@@ -15,24 +15,35 @@ int k; // chessboard size
 int h; // recommended value of the upper bound (akt_min)
 int bestSolution;
 vector<pair<int, int>> bestSolutionMoves;
+vector<TaskInstance> statesToDo;
 
 // compile with "g++ -O3 -fopenmp main.cpp TaskInstance.cpp TaskInstance.h -o cv1.exe"
 int main(int argc, char **argv) {
     const string filename = argv[1];
     cout << filename << endl;
 
-    TaskInstance task;
-    task.readFromFile("./input/" + filename, k, h);
-    task.printTaskInfo(k, h);
+    TaskInstance initTask;
+    initTask.readFromFile("./input/" + filename, k, h);
+    initTask.printTaskInfo(k, h);
 
-    bestSolution = h; // set upper bound (h from file input)
+    bestSolution = h + 1; // set upper bound (h from file input); +1 for the initial position
 
     // measure execution time
     double startTime = omp_get_wtime();
 
-    # pragma omp parallel
-    # pragma omp single
-    recursionParallel(task, task.queenPosition.first, task.queenPosition.second, 1);
+    // create states
+    statesToDo = vector<TaskInstance>();
+    generateStatesRecursively(initTask, initTask.queenPosition.first, initTask.queenPosition.second, 1);
+
+    # pragma omp parallel for
+    for (int i = 0; i < statesToDo.size(); i++) {
+        vector<pair<int, int>> possibleMoves = vector<pair<int, int>>();
+        statesToDo[i].getPossibleMoves(k, possibleMoves);
+
+        for (int j = 0; j < possibleMoves.size(); j++) {
+            recursionSequential(statesToDo[i], possibleMoves[j]);
+        }
+    }
 
     const double finalTime = omp_get_wtime() - startTime;
     cout << endl << "total time: " << finalTime << endl;
@@ -45,26 +56,12 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void recursionParallel(TaskInstance task, int x, int y, int treeLevel) {
+void generateStatesRecursively(TaskInstance task, int x, int y, int treeLevel) {
     task.movesCount++;
     task.madeMoves.emplace_back(x, y); // record the queen movement
 
     if (task.board[x][y] == BLACK_PEON) {
         task.blacksCount--;
-
-        if (task.blacksCount <= 0) {
-            // We've eliminated all the black peons. Is this the best solution?
-            if (task.movesCount < bestSolution) {
-                #pragma omp critical
-                {
-                    if (task.movesCount < bestSolution) {
-                        bestSolution = task.movesCount;
-                        bestSolutionMoves = task.madeMoves;
-                    }
-                };
-            }
-            return;
-        }
     }
 
     // this can't be better than our best solution, don't continue
@@ -76,22 +73,19 @@ void recursionParallel(TaskInstance task, int x, int y, int treeLevel) {
     task.queenPosition.second = y;
     task.board[x][y] = QUEEN;
 
-    vector<pair<int, int>> possibleMoves = vector<pair<int, int>>();
-    task.getPossibleMoves(k, possibleMoves);
+    if (treeLevel < THRESHOLD) {
+        vector<pair<int, int>> possibleMoves = vector<pair<int, int>>();
+        task.getPossibleMoves(k, possibleMoves);
 
-    for (int i = 0; i < possibleMoves.size() - 1; i++) {
-        // limit parallelism with THRESHOLD using "treeLevel", see slides 30 - 32 from 3rd lecture
-        if (treeLevel < THRESHOLD) {
+        for (int i = 0; i < possibleMoves.size(); i++) {
             int newX = possibleMoves[i].first;
             int newY = possibleMoves[i].second;
-
-            #pragma omp task shared(newX, newY)
-            recursionParallel(task, newX, newY, treeLevel + 1);
-        } else {
-            recursionSequential(task, possibleMoves[i]);
+            generateStatesRecursively(task, newX, newY, treeLevel + 1);
         }
+    } else {
+        statesToDo.push_back(task);
+        return;
     }
-    recursionParallel(task, possibleMoves[possibleMoves.size() - 1].first, possibleMoves[possibleMoves.size() - 1].second, treeLevel + 1);
 }
 
 void recursionSequential(TaskInstance task, pair<int, int> queenNewPosition) {
